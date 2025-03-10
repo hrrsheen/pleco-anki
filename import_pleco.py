fdda506b3aa9ee046fd55f981bd07f12d2bfd243
hrrsheen <h.r.r.sheen@gmail.com>
2025-03-10 12:05:03 +1000

    Fixed capitalisation in the pinyin conversion

--- import_pleco.py
+++ import_pleco.py
@@ -1,4 +1,5 @@
 from tones import convert_numeric_sentence
+from anki import collection
 from dataclasses import dataclass
 from typing import Optional
 from xml.etree import ElementTree as ET
@@ -25,14 +26,18 @@ def parse_file(filename: str) -> dict[str, FlashcardBack]:
         dict_card = True if card[1].tag == "dictref" else False
         if dict_card:
             flashcards[headword[0][1]] = parse_dict_card(pron, defn) #TODO create a less hard-coded way of addressing headword elements
+            print(flashcards[headword[0][1]].pron.ljust(7) + ": ")
+            for defn in flashcards[headword[0][1]].defn:
+                print("          " + defn)
+            print('--------------------------------------------------------------------------------------------------')
         else:
             flashcards[headword[0][1]] = parse_user_card(pron, defn)
         
-        print(flashcards[headword[0][1]])
 
 
 def parse_dict_card(pron: str, defn: str) -> FlashcardBack:
-    return FlashcardBack(convert_numeric_sentence(pron), defn)
+    definitions = defn.split("\n")
+    return FlashcardBack(convert_numeric_sentence(pron), definitions)
 
 def parse_user_card(pron: str, defn: str) -> FlashcardBack:
     return FlashcardBack(convert_numeric_sentence(pron), defn)
--- tones.py
+++ tones.py
@@ -12,47 +12,58 @@ TONE_MAP = {
 
 VOWELS = "aeiouü"
 
-WORD_PATTERN = r"([a-z]{2,})([1-5]{1})?"
+WORD_PATTERN = r"([a-zA-Z]{2,})([1-5]{1})?"
 
-def vowel_positions(pinyin: Union[str, list[chr]]) -> dict[chr, int]:
-    present_vowels: dict[chr, int] = {}
+# def vowel_positions(pinyin: Union[str, list[chr]]) -> dict[chr, int]:
+#     present_vowels: dict[chr, int] = {}
 
-    last_vowel: int = 0
-    for i, c in enumerate(pinyin):
-        if c in VOWELS:
-            last_vowel = i
-            present_vowels[c] = i
-    present_vowels['l'] = last_vowel
+#     last_vowel: int = 0
+#     for i, c in enumerate(pinyin):
+#         if c in VOWELS:
+#             last_vowel = i
+#             present_vowels[c] = i
+#     present_vowels['l'] = last_vowel
 
-    return present_vowels
+#     return present_vowels
 
 
 def apply_tone(pinyin: str, tone: int) -> str:
     tone_index = tone - 1
-    new_pinyin = list(pinyin.lower())
-    vowels = vowel_positions(pinyin)
+    new_pinyin: list[chr] = []          # Stores the chatacters to be used in the tonal pinyin.
+    capitalisation: list[int] = []      # Stores the position of each capital character.
+    vowels_present: dict[chr, int] = {} # Stores the position of each vowel in the word.
 
-    # 'a' and 'e' take priprity. There will never be both an 'a' and 'e' in a word.
-    if 'a' in vowels:
-        new_pinyin[vowels['a']] = TONE_MAP['a'][tone_index]
-        return ''.join(new_pinyin)
-    elif 'e' in vowels:
-        new_pinyin[vowels['e']] = TONE_MAP['e'][tone_index]
-        return ''.join(new_pinyin)
-        
+    last_vowel: int = 0
+    for i, c in enumerate(pinyin):
+        # Record the position of any capitalised letters.
+        if c.isupper():
+            capitalisation.append(i)
+        # Record the position of each vowel
+        if c in VOWELS:
+            last_vowel = i
+            vowels_present[c] = i
+        new_pinyin.append(c.lower())
+
+    # 'a' and 'e' take priority. There will never be both an 'a' and 'e' in a word.
+    if 'a' in vowels_present:
+        new_pinyin[vowels_present['a']] = TONE_MAP['a'][tone_index]
+    elif 'e' in vowels_present:
+        new_pinyin[vowels_present['e']] = TONE_MAP['e'][tone_index]
     # In 'ou' or 'uo' combinations, the 'o' always take the tone.
-    if 'o' in vowels and 'u' in vowels:
-        o_pos = vowels['o']
-        u_pos = vowels['u']
+    elif 'o' in vowels_present and 'u' in vowels_present:
+        o_pos = vowels_present['o']
+        u_pos = vowels_present['u']
         space = o_pos - u_pos
 
         if space * space == 1:
             new_pinyin[o_pos] = TONE_MAP['o'][tone_index]
-            return ''.join(new_pinyin)
-    
     # Otherwise the last vowel gets the tone.
-    test_vowel = new_pinyin[vowels['l']]
-    new_pinyin[vowels['l']] = TONE_MAP[test_vowel][tone_index]
+    else:
+        test_vowel = pinyin[last_vowel]
+        new_pinyin[last_vowel] = TONE_MAP[test_vowel][tone_index]
+        
+    for i in capitalisation:
+        new_pinyin[i] = new_pinyin[i].upper()
     return ''.join(new_pinyin)
 
 

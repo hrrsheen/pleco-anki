e976f4b4cf9589f591510792d49acbb92866d6c7
hrrsheen <h.r.r.sheen@gmail.com>
2025-03-15 22:19:17 +1000

    Added the GUI and importing of custom Pleco cards.

--- __init__.py
+++ __init__.py
@@ -1,56 +1,95 @@
-from aqt import mw # import the main window object (mw) from aqt
-from aqt.operations import QueryOp
-from aqt.utils import showInfo, qconnect # import the "show info" tool from utils.py
-from aqt.qt import * # import all of the Qt GUI library
+from dataclasses import asdict, fields, Field
 from functools import partial
 from os.path import dirname
 from pathlib import Path
-from .card_convert import test_import, parse_pleco_xml, FlashcardBack
+from typing import Optional
+
+from aqt import mw  # import the main window object (mw) from aqt
+from aqt.operations import QueryOp
+from aqt.qt import *  # import all of the Qt GUI library
+from aqt.utils import qconnect  # import the "show info" tool from utils.py
+
+from .card_convert import AnkiNotes, CardTemplate, Flashcard, NoteContent, parse_pleco_xml
 from .ui.main_widget import Ui_Dialog
 
-class ImportDialog(QDialog):
-    tr = partial(QCoreApplication.translate, "Dialog")
+tr = partial(QCoreApplication.translate, "Dialog")
 
+class ImportDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.dialog = Ui_Dialog()
         self.dialog.setupUi(self)
         self.connect_signals()
 
+        # Holds the last directory that the user opened when browsing for the Pleco XML file.
         self.last_dir = str(Path.home())
+
+        self.notes_custom: Optional[AnkiNotes] = None   # Note handler for custom user flashcards.
+        self.notes_dict: Optional[AnkiNotes] = None     # Note handler for Pleco dictionary flashcards.
+
+        # Populate the decks menu with options of decks to import to.
+        decks = [deck.name for deck in mw.col.decks.all_names_and_ids()]
+        self.dialog.select_deck.addItems(decks)
     
     def connect_signals(self):
-        self.dialog.button_file.clicked.connect(self.select_file)
-        self.dialog.button_import.clicked.connect(self.perform_import)
+        self.dialog.button_file.clicked.connect(self.select_file)       # Connect the file button to the file browser.
+        self.dialog.button_import.clicked.connect(self.perform_import)  # Connect the import button to the import oprtation.
+        self.dialog.button_cancel.clicked.connect(lambda: self.close()) # Close the plugin UI when "Cancel" is clicked.
 
     def select_file(self):
         tr = partial(QCoreApplication.translate, "Dialog")
         
-        selected_xml, _ = QFileDialog.getOpenFileName(self, self.tr("Open the exported Pleco deck"), self.last_dir, "XML files (*.xml)")
+        selected_xml, _ = QFileDialog.getOpenFileName(self, tr("Open the exported Pleco deck"), self.last_dir, "XML files (*.xml)")
         self.last_dir = dirname(selected_xml)
         if selected_xml:
             self.dialog.line_file.setText(selected_xml)
     
     def perform_import(self):
         pleco_file = self.dialog.line_file.text()
+        deck_name = self.dialog.select_deck.currentText()
+
+        # Set the import operation to run in the background.
         op = QueryOp(
             parent=mw,
-            op=lambda _: parse_pleco_xml(pleco_file),
+            op=lambda _: self.import_pleco(pleco_file, deck_name),
             success=self.import_success,   
         )
-        op.failure(self.import_failure)
-        self.dialog.button_import.setText(self.tr("Importing..."))
+
+        # Style the import button.
+        self.dialog.button_import.setText(tr("Importing..."))
         self.dialog.button_import.setEnabled(False)
+        # Run the import operation.
         op.with_progress().run_in_background()
 
-    def import_success(self, flashcards: dict[FlashcardBack]):
-        self.dialog.button_import.setText(self.tr("Import"))
-        self.dialog.button_import.setEnabled(True)
+    def import_pleco(self, xml_file: str, deck_name: str):
+        print("Importing deck from: " + xml_file + " -> to deck: " + deck_name )
+        flashcards = parse_pleco_xml(xml_file)
+        
+        # Open / create the selected deck.
+        collection = mw.col
+        deck_id = collection.decks.id_for_name(deck_name)
+        if not deck_id:
+            deck = collection.decks.new_deck()
+            deck.name = deck_name
+            collection.decks.add_deck(deck)
+            deck_id = collection.decks.id_for_name(deck_name)
 
-    def import_failure(self, error: Exception):
-        print(type(error))
-        print(error)
-        self.dialog.button_import.setText(self.tr("Import"))
+        # collection.decks.get(deck_id)["mid"]
+        for card in flashcards:
+            if card.dict_type:
+                pass # TODO We'll come back to this.
+            else:
+                # Load the custom NoteType interface only if some actually exist in the import. 
+                if self.notes_custom is None:
+                    note_fields = [f.name for f in fields(card.content)]
+                    self.notes_custom = AnkiNotes.init_from_filenames("CustomPleco", note_fields, [("./front.html", "./back.html")], "./card.css")
+
+                new_note = self.notes_custom.create_note(deck_id, asdict(card.content))
+        
+        return 0
+
+    def import_success(self, status: int):
+        self.dialog.button_import.setText(tr("Import"))
         self.dialog.button_import.setEnabled(True)
 
 
--- card_convert.py
+++ card_convert.py
@@ -1,28 +1,31 @@
-from .string_parsing import contains_pua
-from .tones import convert_numeric_sentence
-from aqt import mw
-from anki import collection
 from dataclasses import dataclass
 from typing import Optional
 from xml.etree import ElementTree as ET
 
+from anki import collection, decks, models, notes
+from aqt import mw
+
+from .string_parsing import contains_pua
+from .tones import convert_numeric_sentence
+
 NOTE_TYPE_NAME = "PlecoImports"
 
 @dataclass
-class Definition:
-    defn: str
-    syntax: Optional[str] = ""
+class NoteContent:
+    headword_sc:    str
+    pron:           str
+    defn:           list[str]
 
 @dataclass
-class FlashcardBack:
-    pron: str
-    defn: list[Definition]
-    needs_check: Optional[bool] = False
+class Flashcard:
+    content:        NoteContent
+    dict_type:      Optional[bool] = False
+    needs_check:    Optional[bool] = False
 
 @dataclass
 class CardTemplate:
-    front_html: str
-    back_html: str
+    front_html:     str
+    back_html:      str
 
     def __post_init__(self):
         with open(self.front_html, "r") as file:
@@ -31,47 +34,13 @@ class CardTemplate:
         with open(self.back_html, "r") as file:
             self.back_html = file.read()
 
-
-def parse_pleco_xml(filename: str) -> dict[str, FlashcardBack]:
-    flashcards: dict[str, FlashcardBack] = {}
-    
-    tree = ET.parse(filename)
-    root = tree.getroot()
-
-    for card in root.find("cards"):
-        back_info: ET.Element = card.find("entry") # The Pleco spec for XML isn't set in stone, so it's best not to use child indices to find tags.
-        headword = [(h.attrib.get("charset"), h.text) for h in back_info.findall("headword")]
-        pron = back_info.find("pron").text
-        defn = back_info.find("defn").text
-        
-        selected_headword = headword[0][1] #TODO select this based on either Simplified or Traditional.
-        # Check if this is a dictionary card.
-        dict_card = True if card.find("dictref") is not None else False
-        if dict_card:
-            flashcards[selected_headword] = parse_dict_card(pron, defn)
-        # Otherwise the card is a custom card.
-        else:
-            flashcards[selected_headword] = parse_user_card(pron, defn)
-            # print(flashcards[selected_headword])
-            # print("----------------------------------------------------------------------------------")
-
-    return flashcards
-        
-
-def parse_dict_card(pron: str, defn: str) -> FlashcardBack:
-    definitions = defn.split("\n")
-    return FlashcardBack(convert_numeric_sentence(pron), definitions, contains_pua(defn))
-
-def parse_user_card(pron: str, defn: str) -> FlashcardBack:
-    return FlashcardBack(convert_numeric_sentence(pron), defn)
-
 class AnkiNotes:
     """
     Manages the creation of Anki notes.
     """
-
     def __init__(self, model_name: str, ordered_fields: list[str], templates: list[CardTemplate], css: str=""):
         self.collection = mw.col
+        self.fields = ordered_fields
 
         model_manager = self.collection.models
         # Create the Note Type (model) if it doesn't already exist.
@@ -107,12 +76,64 @@ class AnkiNotes:
         else:
             model_manager.update_dict(self.model)
 
+    @property
+    def id(self) -> models.NotetypeId:
+        return self.model["id"]
+
+    @classmethod
+    def init_from_filenames(cls, model_name: str, ordered_fields: list[str], templates: list[tuple[str, str]], css_filename: str=""):
+        template_contents = [CardTemplate(front, back) for  front, back in templates]
+        css = ""
+        if css_filename:
+            with open(css_filename, "r") as css_file:
+                css = css_file.read()
+
+        return cls(model_name, ordered_fields, template_contents, css)
+    
+    def create_note(self, deck_id: decks.DeckId, card: dict[str, str]) -> notes.Note:
+        fields = [card[f] for f in self.fields]
+
+        note = self.collection.new_note(self.model)
+        note.fields = fields
+
+        self.collection.add_note(note, deck_id)
+
+        return note
+    
+
+def parse_pleco_xml(filename: str) -> list[Flashcard]:
+    flashcards: list[Flashcard] = []
+    
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    for card in root.find("cards"):
+        back_info: ET.Element = card.find("entry") # The Pleco spec for XML isn't set in stone, so it's best not to use child indices to find tags.
+        headword = [(h.attrib.get("charset"), h.text) for h in back_info.findall("headword")]
+        pron = back_info.find("pron").text
+        defn = back_info.find("defn").text
+        
+        selected_headword = headword[0][1] #TODO select this based on either Simplified or Traditional.
+        # Check if this is a dictionary card.
+        dict_card = True if card.find("dictref") is not None else False
+        if dict_card:
+            flashcards.append(parse_dict_card(selected_headword, pron, defn))
+        # Otherwise the card is a custom card.
+        else:
+            flashcards.append(parse_user_card(selected_headword, pron, defn))
+
+    return flashcards
+        
+
+def parse_dict_card(headword: str, pron: str, defn: str) -> Flashcard:
+    definitions = defn.split("\n")
+    return Flashcard(NoteContent(headword, convert_numeric_sentence(pron), definitions), True, contains_pua(defn))
+
+def parse_user_card(headword: str, pron: str, defn: str) -> Flashcard:
+    return Flashcard(NoteContent(headword, convert_numeric_sentence(pron), defn))
+
 
 def test_import():
-    custom_templ = CardTemplate("./front.html", "./back.html")
-    with open("./card.css", "r") as css_file:
-        css = css_file.read()
-    custom_notes = AnkiNotes("CustomPleco", ["headword_sc", "pron", "defn"], [custom_templ], css)
     cards = parse_pleco_xml("./flash-fixed.xml")
 
 if __name__ == "__main__":
